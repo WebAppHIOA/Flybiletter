1b0484c100d7105a1ee94ab1f78d7785a4e19599
Ginelle <gnlzignacio@gmail.com>
2017-10-19 18:36:56 +0200

    HTML sider til admin (ikke responsiv enda)

--- Flybiletter/Controllers/AdminController.cs
+++ Flybiletter/Controllers/AdminController.cs
@@ -8,8 +8,36 @@ namespace Flybiletter.Controllers
 {
     public class AdminController : Controller
     {
+    
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult Login()
+        {
+            return View();
+        }
+        /*
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return RedirectToAction("Home");
+        }*/
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Departure()
+        {
+            return View();
+        }
+
+        public ActionResult Airport()
+        {
+            return View();
+        }
+
+
+        public ActionResult Order()
         {
             return View();
         }
