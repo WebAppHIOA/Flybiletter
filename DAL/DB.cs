c62039799fe42ae3e755b191f72843938e843ef4
Ginelle <gnlzignacio@gmail.com>
2017-10-28 18:17:31 +0200

    Siste endringer med responsiv adminsider

--- DAL/DB.cs
+++ DAL/DB.cs
@@ -478,7 +478,7 @@ namespace DAL
 
             using (var db = new AirportContext())
             {
-                var total = db.Departure.Count();
+                var total = db.Order.Count();
                 return total;
             }
         }
